94f1a79c9fe645f0d00b39b050d9021909b5ba55
Pedro Barbosa <pedro_b@pop-os.localdomain>
2024-03-30 16:14:46 -0300

    implement custom tag filtering to random test selection

--- spec/composition/composition_spec.rb
+++ spec/composition/composition_spec.rb
@@ -1,4 +1,4 @@
-describe 'Ruby on Rails' do
+describe 'Ruby on Rails', :composition do
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
   it { expect(fruit).to eq('banana').or eq('tomato') } # In this case, as it samples through the array, it may or may
   # may not pass.
--- spec/custom/custom_spec.rb
+++ spec/custom/custom_spec.rb
@@ -1,3 +1,3 @@
-describe 18, 'Custom Matcher' do
+describe 18, 'Custom Matcher', :custom do
   it { is_expected.to be_a_multiple_of(3) }
 end
