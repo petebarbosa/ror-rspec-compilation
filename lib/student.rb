794e22c2473888d3fc851f7831fee6d98bfcb9bc
Pedro Barbosa <pedro_b@pop-os.localdomain>
2024-04-03 14:06:20 -0300

    Implemenet more stub methods tests

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,12 @@ class Student # :nodoc:
   def finished?(course)
     # some code that should return true || false
   end
+
+  def foo(arg)
+    # some code that receives arguments
+  end
+
+  def bar
+    # some code
+  end
 end
--- spec/stubs/stubs_spec.rb
+++ spec/stubs/stubs_spec.rb
@@ -7,10 +7,44 @@ describe 'stubs', :stubs do
   it '#finished' do
     student = Student.new
     course = Course.new
+
     allow(student).to receive(:finished?)
       .with(an_instance_of(Course))
       .and_return(true)
     course_finished = student.finished?(course)
+
     expect(course_finished).to be_truthy
+  end
+
+  it 'if/elsif' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :this
+        'got this'
+      elsif arg == :that
+        'got that'
+      end
+    end
+
+    expect(student.foo(:this)).to eq('got this')
+    expect(student.foo(:that)).to eq('got that')
+  end
+
+  it 'multiple instances' do
+    student = Student.new
+    another_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(Student).to be_truthy
+  end
+
+  it 'testing erros' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect { student.bar }.to raise_error(RuntimeError)
   end
 end
