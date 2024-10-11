7a37484d3df74229f67a1bd9616ba4a176a16440
Pedro_Barbosa <pedrovbb89@gmail.com>
2024-10-11 00:11:59 -0300

    feat: implement mocks

---
+++ spec/mocks/mocks_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'student'
+require 'course'
+
+describe 'Mocks', :mocks do
+  it '#bar' do
+    # setup
+    student = Student.new
+
+    # verify
+    expect(student).to receive(:bar)
+
+    # exercise
+    student.bar
+  end
+
+  it 'args' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+
+  it 'repetition' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    2.times { student.foo(123) }
+  end
+
+  it 'return' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    puts student.foo(123)
+  end
+end
