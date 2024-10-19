1dfe3b34480e3d80f92122723826a46d364ce38b
petebarbosa <pedrovbb89@gmail.com>
2024-10-18 22:48:29 -0300

    feat: null_object example

--- spec/test_double/user_spec.rb
+++ spec/test_double/user_spec.rb
@@ -12,4 +12,17 @@ describe 'double test' do
     allow(user).to receive_messages(name: 'Jack', password: 'secret')
     user.name
   end
+
+  # Imagine you'are testint a controller that sends an email when a task is created.
+  # You have a service class that called `EmailSender`, but you don't
+  # care about the actual behavior of the e-mail sendindg logic.
+  # You just want to make sure it's beeing called. It's logic is tested elsewhere.
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    user.abc
+    # Instead of an error, the test will pass.
+  end
 end
