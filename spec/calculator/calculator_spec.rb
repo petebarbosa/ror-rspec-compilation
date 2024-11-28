acd8ea422180de147ff20c0259780aa9e8258c20
petebarbosa <pedrovbb89@gmail.com>
2024-11-28 19:09:46 -0300

    feat: add explanations

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -2,13 +2,19 @@
 
 require 'calculator'
 
+# When we use the class name with our describe, the application will automatically
+# instantiate our class, so we do not need to call it by:
+# cacl = Calculator.new
 describe Calculator, 'Crazy calculator class' do
+
   # The idea with context is to organize all tests within, well, the same
   # context. If we're testing, in our calculator, everything related to a
   # sum, we can simply set all of the into the '#sum' context.
-
+  # Remember that, describing context with just ou test with just # or . is not set in stone
+  # if it's needed to add more information, avoid using it.
   context '#sum' do
     it 'positive numbers' do
+      # Subject is our automatically instantiated Calculator.new
       result = subject.sum(5, 7)
       expect(result).to eq(12)
     end
@@ -22,6 +28,11 @@ describe Calculator, 'Crazy calculator class' do
       result = subject.sum(-5, -7)
       expect(result).to eq(-12)
     end
+
+    # When implementing our tests, we can choose to write it but not implement at the
+    # time. Like a memo. All we need to do it not implement it's body. It'll work, but show
+    # as pending.
+    it 'test to add later'
   end
 
   context '#div' do
